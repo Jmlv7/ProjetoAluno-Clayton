5793d6d62b7e8927a3ffe68a56b00d0dfcd97cb9
Jmlv7 <165213783+Jmlv7@users.noreply.github.com>
2025-02-10 19:02:35 -0300

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Student {
+    private String nome;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+
+    public Student(String nome, double nota1, double nota2, double nota3) {
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public double notaFinal() {
+        return nota1 + nota2 + nota3;
+    }
+
+    public String verificarAprovacao() {
+        double notaFinal = notaFinal();
+        if (notaFinal >= 60.0) {
+            return "NOTA FINAL = " + String.format("%.2f", notaFinal) + "\nPASS";
+        } else {
+            double pontosFaltando = 60.0 - notaFinal;
+            return "NOTA FINAL = " + String.format("%.2f", notaFinal) +
+                    "\nFAILED\nFALTARAM " + String.format("%.2f", pontosFaltando) + " PONTOS";
+        }
+    }
 }
