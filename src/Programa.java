0d9214ecd50b6b5cd41b5665079fe935e4149a1c
Jmlv7 <165213783+Jmlv7@users.noreply.github.com>
2025-02-10 19:02:09 -0300

    Update Programa.java

--- src/Programa.java
+++ src/Programa.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Programa {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nome do aluno:");
+        String nome = sc.nextLine();
+        System.out.println("Digite as três notas:");
+        double nota1 = sc.nextDouble();
+        double nota2 = sc.nextDouble();
+        double nota3 = sc.nextDouble();
+
+        Student aluno = new Student(nome, nota1, nota2, nota3);
+
+        System.out.println(aluno.verificarAprovacao());
+
+        sc.close();
+    }
 }
